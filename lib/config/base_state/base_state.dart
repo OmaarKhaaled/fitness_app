eee53f14a93131172b7e41453ea3a4044e5128bd
khaled Ahmed <khaledahm79@gmail.com>
2026-04-11 20:00:26 +0200

    feat: add BaseState class for standardized state management

--- lib/config/base_state/base_state.dart
+++ lib/config/base_state/base_state.dart
@@ -1,13 +1,13 @@
 import 'package:equatable/equatable.dart';
 
 class BaseState<T> extends Equatable {
-  final bool isError;
+  final String? errorMessage;
   final bool isLoading;
   final T? data;
 
-  const BaseState({this.isError = false, this.isLoading = false, this.data});
+  const BaseState({this.errorMessage, this.isLoading = false, this.data});
 
-  BaseState<T> copyWith({bool? isError, bool? isLoading, T? data}) {
+  BaseState<T> copyWith({String? errorMessage, bool? isLoading, T? data}) {
     return BaseState<T>(
       isError: isError ?? this.isError,
       isLoading: isLoading ?? this.isLoading,
