f0f74f51871b26e28d7a3a58025d7a45165ad13f
Islam Ramzy <islamramzy2@gmail.com>
2026-04-23 23:23:33 +0200

    fix(ui): Fix merging issues

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,34 +1,24 @@
-<<<<<<< feature/app-sections
 import 'package:fitness_app/features/home/views/screens/home_screen.dart';
-
-import '../constants/app_routes_constants.dart';
-=======
-
 import 'package:fitness_app/core/constants/app_routes_constants.dart';
 import 'package:fitness_app/features/auth/login/presentation/views/screens/login_screen.dart';
 import 'package:fitness_app/features/auth/register/presentation/views/screens/register_additional_info_screen.dart';
 import 'package:fitness_app/features/auth/register/presentation/views/screens/register_screen.dart';
->>>>>>> development
 import '../../features/on_boarding/presentation/pages/on_boarding_page.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static GoRouter get router => GoRouter(
-<<<<<<< feature/app-sections
-    initialLocation: AppRoutesConstants.homeRoute,
-=======
     initialLocation: AppRoutesConstants.registerRoute,
->>>>>>> development
     routes: [
       GoRoute(
         path: AppRoutesConstants.onboardingRoute,
         builder: (context, state) => const OnBoardingPage(),
       ),
       GoRoute(
-<<<<<<< feature/app-sections
         path: AppRoutesConstants.homeRoute,
         builder: (context, state) => const HomeScreen(),
-=======
+      ),
+      GoRoute(
         path: AppRoutesConstants.registerRoute,
         builder: (context, state) => const RegisterScreen(),
       ),
@@ -39,7 +29,6 @@ class AppRouter {
       GoRoute(
         path: AppRoutesConstants.loginRoute,
         builder: (context, state) => const LoginScreen(),
->>>>>>> development
       ),
     ],
   );
