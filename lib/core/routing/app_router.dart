b8849aafe834a4cc36464a600d31e91c3f6b20d7
omar khaled <omarkhaled10102004@gmail.com>
2026-04-26 19:15:55 +0300

    feat(meals): Add meal recommendations page and endpoints, update translations

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -3,6 +3,7 @@ import 'package:fitness_app/features/auth/forget_password/presentation/view/scre
 import 'package:fitness_app/features/home/views/screens/home_screen.dart';
 import 'package:fitness_app/features/auth/register/presentation/views/screens/register_additional_info_screen.dart';
 import 'package:fitness_app/features/auth/register/presentation/views/screens/register_screen.dart';
+import 'package:fitness_app/features/meals/presentation/view/pages/meals_recommendation_page.dart';
 import '../../features/on_boarding/presentation/pages/on_boarding_page.dart';
 import 'package:fitness_app/features/auth/login/presentation/pages/login_page.dart';
 import 'package:go_router/go_router.dart';
@@ -36,6 +37,10 @@ class AppRouter {
         name: AppRoutesConstants.forgetPasswordRoute,
         path: AppRoutesConstants.forgetPasswordRoute,
         builder: (context, state) => const ForgetPasswordScreen(),
+      ),
+      GoRoute(
+        path: AppRoutesConstants.mealsRecommendationRoute,
+        builder: (context, state) => const MealsRecommendationPage(),
       ),
     ],
   );
